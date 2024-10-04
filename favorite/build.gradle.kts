916170fca0f80ea03e007a62ada6ed5a37f3d2d0
mumuhkustino <mumuhkustino@gmail.com>
2024-10-04 08:26:22 +0700

    - Fix implementation obfuscation in all module

--- favorite/build.gradle.kts
+++ favorite/build.gradle.kts
@@ -12,8 +12,19 @@ android {
     }
 
     buildTypes {
+        debug {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
         release {
             isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
 
